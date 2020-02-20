2285f1a698eaac9f7cb5d68e7666aec73674469b
tolyod <anatoliy.poloz@gmail.com>
2020-02-20 16:42:07 +0300

    options modification

--- src/Specs.php
+++ src/Specs.php
@@ -3,13 +3,17 @@
 namespace Gendiff;
 
 const GENDIFFDOC = <<<DOC
+gendiff -h
+
 Generate diff
 
 Usage:
   gendiff (-h|--help)
   gendiff (-v|--version)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
 
 Options:
   -h --help                     Show this screen
   -v --version                  Show version
+  --format <fmt>                Report format [default: pretty]
 DOC;
