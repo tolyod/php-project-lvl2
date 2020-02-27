3c0aea5c5a73aaecca2539950371b57bd75e423f
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-02-28 02:34:14 +0300

    tests added

---
+++ tests/DifferTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Differ\tests;
+
+use \PHPUnit\Framework\TestCase;
+use function Differ\genDiff;
+
+class DifferTest extends TestCase
+{
+    /**
+    * @dataProvider additionProvider
+    */
+    public function testGenDiff($expectedFileName, $testFileName1, $testFileName2)
+    {
+        $expectedFilePath = $this->getFilePath($expectedFileName);
+        $testFilePath1 = $this->getFilePath($testFileName1);
+        $testFilePath2 = $this->getFilePath($testFileName2);
+        $diff = genDiff($testFilePath1, $testFilePath2);
+        $expectedResult = file_get_contents($expectedFilePath);
+        $this->assertEquals($expectedResult, $diff);
+    }
+
+    public function getFilePath($fileName)
+    {
+        return __DIR__ . "/fixtures/$fileName";
+    }
+
+    public function additionProvider()
+    {
+        return [
+            ['flatJsonExpected.out', 'flatJsonBefore.json', 'flatJsonAfter.json'],
+        ];
+    }
+}
