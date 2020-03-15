d667a00f42681eedc8c07ce8f692bad226151db7
anatoliy.poloz <anatoliy.poloz@gmail.com>
2020-03-15 04:46:52 +0300

    step#7 formaters separetion added plus new format 'plain'

---
+++ src/Formatters/Plain.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Differ\Formatters\Plain;
+
+function formatValue($value)
+{
+    if (is_object($value)) {
+        return "complex value";
+    }
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    }
+    if (is_null($value)) {
+        return 'null';
+    }
+    return $value;
+}
+
+function getStringBuilders($parentName)
+{
+    return [
+        "added" => function ($elem) use ($parentName) {
+            [ "name" => $name, "value" => $value ] = $elem;
+            return "Property '" . $parentName . $name . "' was added with value: '" . formatValue($value) . "'";
+        },
+        "deleted" => function ($elem) use ($parentName) {
+            [ "name" => $name ] = $elem;
+            return "Property '" . $parentName . $name . "' was removed";
+        },
+        "node" => function ($elem, $renderFn) use ($parentName) {
+            [ "name" => $name, "children" => $children ] = $elem;
+            return $renderFn($children, "{$parentName}{$name}.");
+        },
+        "updated" => function ($elem) use ($parentName) {
+            [ "name" => $name, "beforeValue" => $beforeValue, "afterValue" => $afterValue ] = $elem;
+            return "Property '" . $parentName . $name . "' was changed. From '"
+                . formatValue($beforeValue) . "' to '" . formatValue($afterValue) . "'";
+        }
+     ];
+}
+
+function renderAst($data, $parentName = "")
+{
+    $selfCallback = __FUNCTION__;
+    $filtered = array_filter($data, function ($elem) {
+        return $elem['type'] !== 'unchanged';
+    });
+    $stringBuilders = getStringBuilders($parentName);
+
+    $strings = array_map(function ($elem) use ($stringBuilders, $selfCallback) {
+        ['type' => $type] = $elem;
+        return $stringBuilders[$type]($elem, $selfCallback);
+    }, $filtered);
+    return join("\n", $strings);
+}
---
+++ src/Formatters/Pretty.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Differ\Formatters\Pretty;
+
+function buildOffsetedJsonFromObject($value, $offset)
+{
+    $lines = explode("\n", json_encode($value, JSON_PRETTY_PRINT));
+    return join(
+        "\n",
+        array_map(
+            function ($inputLine) use ($offset) {
+                $line = preg_replace("/\"/", "", $inputLine);
+                if (trim($line) === "{") {
+                    return $line;
+                }
+                return $offset . $line;
+            },
+            $lines
+        )
+    );
+}
+
+function formatValue($value, $level = 1)
+{
+    $offset = makeOffset($level);
+    if (is_object($value)) {
+        return buildOffsetedJsonFromObject($value, $offset);
+    }
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    } elseif (is_null($value)) {
+        return 'null';
+    } else {
+        return $value;
+    }
+}
+
+function makeOffset($level)
+{
+    return str_repeat('    ', $level);
+}
+
+function getStringBuilders($level)
+{
+    $opOffset = substr(makeOffset($level), 0, -2);
+    return [
+        "added" => function ($elem) use ($opOffset, $level) {
+            [ "name" => $name, "value" => $value ] = $elem;
+            return "{$opOffset}+ $name: " . formatValue($value, $level);
+        },
+        "deleted" => function ($elem) use ($opOffset, $level) {
+            [ "name" => $name, "value" => $value ] = $elem;
+            return "{$opOffset}- $name: " . formatValue($value, $level);
+        },
+        "unchanged" => function ($elem) use ($opOffset, $level) {
+            [ "name" => $name, "value" => $value ] = $elem;
+            return "{$opOffset}  $name: " . formatValue($value, $level);
+        },
+        "node" => function ($elem, $renderFn) use ($opOffset, $level) {
+            [ "name" => $name, "children" => $children ] = $elem;
+            return "{$opOffset}  $name: {$renderFn($children, $level + 1)}";
+        },
+        "updated" => function ($elem) use ($opOffset) {
+            [ "name" => $name, "beforeValue" => $beforeValue, "afterValue" => $afterValue ] = $elem;
+            return "{$opOffset}+ $name: $afterValue\n{$opOffset}- $name: $beforeValue";
+        }
+     ];
+}
+
+function renderAst($data, $level = 1)
+{
+    $selfCallback = __FUNCTION__;
+    $stringBuilders = getStringBuilders($level);
+    $curOffset = makeOffset($level - 1);
+    $firstLine = "{";
+    $lastLine = "$curOffset}";
+    $strings =  array_map(
+        function ($elem) use ($stringBuilders, $selfCallback, $level) {
+            [
+             "type" => $type,
+             "name" => $name,
+            ] = $elem;
+            return $stringBuilders[$type]($elem, $selfCallback);
+        },
+        $data
+    );
+    array_unshift($strings, $firstLine);
+    array_push($strings, $lastLine);
+    return join("\n", $strings);
+}
